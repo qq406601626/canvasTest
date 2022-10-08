d18593820d847824a176d98633b9aaca167ccda5
guoshuai <406601626@qq.com>
2022-10-08 11:08:35 +0800

    修改

--- src/MovedText/index.js
+++ src/MovedText/index.js
@@ -3,20 +3,41 @@ class Stage {
         this.mouseX = 0
         this.mouseY = 0
         this.ctx = null
+        this.textPoor = null
         this.init()
     }
 
     init() {
         const {clientWidth, clientHeight} = document.documentElement
         const cvs = document.querySelector('#cvs')
-        cvs.width = clientWidth
-        cvs.height = clientHeight
+        this.cvsWidth = clientWidth
+        this.cvsHeight = clientHeight
+        cvs.width = this.cvsWidth
+        cvs.height = this.cvsHeight
         this.ctx = cvs.getContext('2d')
+        cvs.addEventListener('mousemove',(e)=>{
+            this.mouseX = e.clientX
+            this.mouseY = e.clientY
+        })
+        cvs.addEventListener('click',(e)=>{
+            this.render()
+        })
     }
+
+    render(){
+        this.ctx.clearRect(0,0,this.cvsWidth,this.cvsHeight)
+        this.textPoor.textList.forEach((text,index)=>{
+            text.setPosition(this.mouseX,this.mouseY,index)
+            text.draw()
+        })
+    }
+
 }
 
 class TextPoor {
-    constructor() {
+    constructor(stage) {
+        this.stage = stage
+        this.stage.textPoor = this
         this.textList = []
         this.textMap = {}
         this.textId = 0
@@ -24,17 +45,20 @@ class TextPoor {
 
     addText(text) {
         this.textId += 1
+        text.stage = this.stage
+        text.id = this.textId
         this.textList.push(text)
         this.textMap[this.textId] = text
     }
 }
 
 class Text {
-    constructor(text, stage) {
-        this.stage = stage
+    constructor(text) {
+        this.id = null
+        this.stage = null
         this.text = text
         this.x = 0
-        this.y = 0
+        this.y = 30
         this.fontSize = 18
     }
 
@@ -44,6 +68,26 @@ class Text {
     }
 
     draw() {
+        this.stage.ctx.beginPath()
+        this.stage.ctx.fillStyle = '#00ff00'
+        this.stage.ctx.font = `${this.fontSize}px 微软雅黑`
         this.stage.ctx.fillText(this.text, this.x, this.y)
     }
 }
+const stage = new Stage()
+const textPoor = new TextPoor(stage)
+const textH = new Text('H')
+const textE = new Text('E')
+const textL1 = new Text('L')
+const textL2 = new Text('L')
+const textO = new Text('O')
+textPoor.addText(textH)
+textPoor.addText(textE)
+textPoor.addText(textL1)
+textPoor.addText(textL2)
+textPoor.addText(textO)
+const run = ()=>{
+    stage.render()
+    window.requestAnimationFrame(run)
+}
+// run()
